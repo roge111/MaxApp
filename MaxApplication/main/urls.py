ee7b93d6db199435464bb4e3cc244d1b0d0b77a2
roge111 <batarginegor@yandex.ru>
2025-11-14 19:08:53 +0300

    Добавил обработку путей для регистрации, авторизации и выхода из ситемы

--- MaxApplication/main/urls.py
+++ MaxApplication/main/urls.py
@@ -6,4 +6,7 @@ from . import views
 urlpatterns = [
     path('', views.main, name='main'),
     path('yandex_gpt/', include('ya_gpt.urls')),
+    path('register/', views.register, name='register'),
+    path('logIn/', views.logIn, name='logIn'),
+    path('logout/', views.logout, name='logout')
 ]
